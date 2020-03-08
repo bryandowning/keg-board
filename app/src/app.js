45c9f5efb8b5fd2ecd58f2ab86ea733d610fe9a9
Jeremy White <jeremy.white@volvocars.com>
2020-03-08 15:42:32 -0700

    Change back to node 10

--- app/src/app.js
+++ app/src/app.js
@@ -1,34 +1,11 @@
-/*
- * Using a PIR motion sensor with Pi
- *
- */
+const Gpio = require('pigpio').Gpio;
 
-var five = require('johnny-five');
-var Raspi = require("raspi-io").RaspiIO;
-var board = new five.Board({
-  io: new Raspi()
+const motion = new Gpio(7, {mode: Gpio.INPUT, alert:true});
+
+console.log("starting")
+
+motion.on('alert', (level, tick) => {
+	console.log("ALERT level: " + level + " tick: " + tick);
 });
 
-board.on('ready', function() {
-	console.log('board is ready');
-
-	// Create a new `motion` hardware instance.
-	var motion = new five.IR.Motion('P1-7'); //pin 7 (GPIO 4)
-
-	// 'calibrated' occurs once, at the beginning of a session,
-	motion.on('calibrated', function() {
-		console.log('calibrated');
-	});
-
-	// 'motionstart' events are fired when the 'calibrated'
-	// proximal area is disrupted, generally by some form of movement
-	motion.on('motionstart', function() {
-		console.log('motionstart');
-	});
-
-	// 'motionend' events are fired following a 'motionstart' event
-	// when no movement has occurred in X ms
-	motion.on('motionend', function() {
-		console.log('motionend');
-	});
-});
+console.log("started")
