d6956c416b461f0f6285a7fe4a4e3bce3c3a6b96
Jeremy White <jeremy.white@volvocars.com>
2020-03-22 15:36:49 -0700

    Convert header to flexbox

--- ui-server/src/Header.js
+++ ui-server/src/Header.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div className="HeaderContainer clearfix">
+    <div className="HeaderContainer">
       <div className="HeaderLeft">
-      <a href="admin/admin.php"><img src="" height="100" alt="" /></a>
+        <a href="admin/admin.php"><img src="" height="100" alt="" /></a>
       </div>
       <div className="HeaderCenter">
           <h1 id="HeaderTitle">Jeremy's Beers</h1>
       </div>
       <div className="HeaderRight">
-        <a href="http://www.raspberrypints.com"><img src="img/RaspberryPints.png" height="100" alt="" /></a>
+        <a href="http://www.raspberrypints.com"><img src={require('./img/RaspberryPints.png')} height="100" alt="" /></a>
       </div>
     </div>
   )
