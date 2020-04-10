0016ca40e971a5e5ad52ddf229a9030475cce5fc
Jeremy White <jeremy.white@volvocars.com>
2020-04-10 11:04:28 -0700

    Update beer info

--- ui-server/src/App.js
+++ ui-server/src/App.js
@@ -13,9 +13,9 @@ function App() {
   //   { id: 5, beername: "TexanBrew Flanders", style: "Flanders Ale", notes: "The beer is aged in the barrel for at least one year", og: 1.052, fg: 1.013, srm: 16, ibu: 0, startAmount: 5, amountPoured: 3.9, remainAmount: 1.1, tapNumber: 5 }
   // ]
   const beers = [
-    { id: 1, beername: "Rauchbier", style: "Rauchbier", notes: "Smokey, amber German style of lager", og: 1.050, fg: 1.017, srm: 14, ibu: 22, startAmount: 5, amountPoured: 0.2, remainAmount: 4.8, tapNumber: 1},
+    { id: 1, beername: "Cherrywood Smoked Lager", style: "Rauchbier", notes: "Smokey, amber German style of lager", og: 1.050, fg: 1.017, srm: 14, ibu: 22, startAmount: 5, amountPoured: 0.2, remainAmount: 4.8, tapNumber: 1},
     { id: 2, beername: "EMPTY", style: "N/A", notes: "", og: 1.0, fg: 1.0, srm: 0, ibu: 0, startAmount: 0, amountPoured: 0, remainAmount: 0, tapNumber: 2},
-    { id: 3, beername: "HQ Big Barrel Brown Ale", style: "American Brown Ale", notes: "American brown ale, originally meant to be aged in an oak barrel", og: 1.062, fg: 1.012, srm: 19, ibu: 31, startAmount: 5, amountPoured: 1, remainAmount: 4, tapNumber: 3},
+    { id: 3, beername: "Modified Janet's Brown", style: "American Brown Ale", notes: "Slightly modified Janet's Brown recipe, adapted for liquid malt extract and a less agressive hop schedule", og: 1.062, fg: 1.012, srm: 19, ibu: 31, startAmount: 5, amountPoured: 1, remainAmount: 4, tapNumber: 3},
     { id: 4, beername: "Old Rasputin Clone", style: "Imperial Stout", notes: "Inspired by the Old Rasputin recipe, with slightly modified grain bill", og: 1.096, fg: 1.028, srm: 31, ibu: 29, startAmount: 5, amountPoured: 0.1, remainAmount: 4.9, tapNumber: 4},
     { id: 5, beername: "EMPTY", style: "N/A", notes: "", og: 1.0, fg: 1.0, srm: 0, ibu: 0, startAmount: 0, amountPoured: 0, remainAmount: 0, tapNumber: 5}
   ]
