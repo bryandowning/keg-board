0d2a3fc0d056918376fc96d94cfb7931f83933d8
Bryan Downing <bryan.downing@idean.com>
2020-04-14 11:33:35 -0700

    Add beer util function to get a color pair

--- ui-server/utils/beer.js
+++ ui-server/utils/beer.js
@@ -38,3 +38,60 @@ export function calcABV(og, fg) {
   const abv = ((76.08 * (og - fg)) / (1.775 - og)) * (fg / 0.794);
   return abv.toFixed(1);
 }
+
+/* Beer colors indexed by SRM */
+export const beerColors = [
+  '#FFE699',
+  '#FFE699',
+  '#FFD878',
+  '#FFCA5A',
+  '#FFBF42',
+  '#FBB123',
+  '#F8A600',
+  '#F39C00',
+  '#EA8F00',
+  '#E58500',
+  '#DE7C00',
+  '#D77200',
+  '#CF6900',
+  '#CB6200',
+  '#C35900',
+  '#BB5100',
+  '#B54C00',
+  '#B04500',
+  '#A63E00',
+  '#A13700',
+  '#9B3200',
+  '#952D00',
+  '#8E2900',
+  '#882300',
+  '#821E00',
+  '#7B1A00',
+  '#771900',
+  '#701400',
+  '#6A0E00',
+  '#660D00',
+  '#5E0B00',
+  '#5A0A02',
+  '#600903',
+  '#520907',
+  '#4C0505',
+  '#470606',
+  '#420607',
+  '#3D0708',
+  '#370607',
+  '#2D0607',
+  '#1F0506',
+];
+
+/**
+ * Get the hex color of a beer based on its SRM
+ * @param   {Number} srm The SRM of a beer
+ * @returns {string}     The hex color of the beer
+ */
+export function getBeerColorPair(srm) {
+  srm = Math.min(Math.max(srm, 1), 40);
+  const beerColor = srm ? beerColors[Math.round(srm)] : beerColors[0];
+  const textColor = srm > 13 ? beerColors[0] : beerColors[beerColors.length - 1];
+  return [textColor, beerColor];
+}
