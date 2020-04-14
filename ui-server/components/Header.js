d0d76f3032197a21698537249665dbbb648b8b91
Bryan Downing <bryan.downing@idean.com>
2020-04-14 12:20:05 -0700

    Add initial header styles

--- ui-server/components/Header.js
+++ ui-server/components/Header.js
@@ -1,5 +1,44 @@
+import styled from 'styled-components';
+
+import { vw } from '../styles/helpers';
+
+const StyledHeader = styled.header`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: ${vw(200)};
+  min-height: 75px;
+  background-image: url('/images/headers/beer.jpg');
+  background-repeat: no-repeat;
+  background-position: center bottom;
+  background-size: cover;
+
+  h1 {
+    position: relative;
+    z-index: 1;
+    margin: 0;
+    color: #470606;
+  }
+
+  &:after {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: block;
+    content: '';
+    background-color: rgba(255, 230, 153, 0.2);
+  }
+`;
+
 const Header = () => {
-  return <h1>Jeremy&rsquo;s Beers</h1>;
+  return (
+    <StyledHeader>
+      <h1>Jeremy&rsquo;s Beers</h1>
+    </StyledHeader>
+  );
 };
 
 export default Header;
