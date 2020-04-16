9782bc0b69b28dcdaf5a66325d4c07e140066a28
Bryan Downing <bryan.downing@idean.com>
2020-04-16 11:28:05 -0700

    Drastically simplify type resizing hook
    This hook is way more scalable because it doesn't require state and a ref for each type style

--- ui-server/pages/styleguide.js
+++ ui-server/pages/styleguide.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import Swatch from '../components/Swatch';
@@ -6,7 +6,19 @@ import Swatch from '../components/Swatch';
 import { beerColors } from '../utils/beer';
 
 import * as colors from '../styles/colors';
-import { typeBodyLead, typeBodySupplemental, typeLabel, typeStat } from '../styles/typography';
+import {
+  typeBodyLead,
+  typeBodySupplemental,
+  typeLabel,
+  typeStat,
+  typeSizeBodyXXS,
+  typeSizeBodyXS,
+  typeSizeBodyS,
+  typeSizeBodyM,
+  typeSizeBodyL,
+  typeSizeBodyXL,
+  typeSizeBodyXXL,
+} from '../styles/typography';
 
 const StyledGuide = styled.div`
   padding: 0 2em;
@@ -28,21 +40,37 @@ const Label = styled.p`
   ${typeLabel()}
 `;
 
+const TypeSizeBodyXXS = styled.div`
+  ${typeSizeBodyXXS}
+  margin-bottom: 1em;
+`;
+const TypeSizeBodyXS = styled.div`
+  ${typeSizeBodyXS}
+  margin-bottom: 1em;
+`;
+const TypeSizeBodyS = styled.div`
+  ${typeSizeBodyS}
+  margin-bottom: 1em;
+`;
+const TypeSizeBodyM = styled.div`
+  ${typeSizeBodyM}
+  margin-bottom: 1em;
+`;
+const TypeSizeBodyL = styled.div`
+  ${typeSizeBodyL}
+  margin-bottom: 1em;
+`;
+const TypeSizeBodyXL = styled.div`
+  ${typeSizeBodyXL}
+  margin-bottom: 1em;
+`;
+const TypeSizeBodyXXL = styled.div`
+  ${typeSizeBodyXXL}
+  margin-bottom: 1em;
+`;
+
 function Styleguide() {
-  const {
-    typeRefs: { h1, h2, h3, h4, lead, primary, supplemental, stat, label },
-    typeSizes: {
-      h1Size,
-      h2Size,
-      h3Size,
-      h4Size,
-      leadSize,
-      primarySize,
-      supplementalSize,
-      statSize,
-      labelSize,
-    },
-  } = useTypeResizer();
+  const typeSizes = useTypeSizing();
 
   // Sort colors alphabetically by name
   const colorsSorted = Object.entries(colors).sort(([nameA], [nameB]) => {
@@ -75,49 +103,87 @@ function Styleguide() {
 
       <h2>Typography</h2>
 
+      <h3>Body Type Scale</h3>
+
+      <TypeSizeBodyXXS className="typeSizerTarget" data-name="typeSizeBodyXXS">
+        typeSizeBodyXXS: {typeSizes.typeSizeBodyXXS}
+        <br />
+        The quick brown fox jumps over the lazy dog.
+      </TypeSizeBodyXXS>
+      <TypeSizeBodyXS className="typeSizerTarget" data-name="typeSizeBodyXS">
+        typeSizeBodyXS: {typeSizes.typeSizeBodyXS}
+        <br />
+        The quick brown fox jumps over the lazy dog.
+      </TypeSizeBodyXS>
+      <TypeSizeBodyS className="typeSizerTarget" data-name="typeSizeBodyS">
+        typeSizeBodyS: {typeSizes.typeSizeBodyS}
+        <br />
+        The quick brown fox jumps over the lazy dog.
+      </TypeSizeBodyS>
+      <TypeSizeBodyM className="typeSizerTarget" data-name="typeSizeBodyM">
+        typeSizeBodyM: {typeSizes.typeSizeBodyM}
+        <br />
+        The quick brown fox jumps over the lazy dog.
+      </TypeSizeBodyM>
+      <TypeSizeBodyL className="typeSizerTarget" data-name="typeSizeBodyL">
+        typeSizeBodyL: {typeSizes.typeSizeBodyL}
+        <br />
+        The quick brown fox jumps over the lazy dog.
+      </TypeSizeBodyL>
+      <TypeSizeBodyXL className="typeSizerTarget" data-name="typeSizeBodyXL">
+        typeSizeBodyXL: {typeSizes.typeSizeBodyXL}
+        <br />
+        The quick brown fox jumps over the lazy dog.
+      </TypeSizeBodyXL>
+      <TypeSizeBodyXXL className="typeSizerTarget" data-name="typeSizeBodyXXL">
+        typeSizeBodyXXL: {typeSizes.typeSizeBodyXXL}
+        <br />
+        The quick brown fox jumps over the lazy dog.
+      </TypeSizeBodyXXL>
+
       <h3>Headings</h3>
-      <h1 ref={h1}>H1, {h1Size}</h1>
-      <h2 ref={h2}>H2, {h2Size}</h2>
-      <h3 ref={h3}>H3, {h3Size}</h3>
-      <h4 ref={h4}>H4, {h4Size}</h4>
+      <h1 className="typeSizerTarget" data-name="typeHeadingH1">
+        H1, {typeSizes.typeHeadingH1}
+      </h1>
+      <h2 className="typeSizerTarget" data-name="typeHeadingH2">
+        H2, {typeSizes.typeHeadingH2}
+      </h2>
+      <h3 className="typeSizerTarget" data-name="typeHeadingH3">
+        H3, {typeSizes.typeHeadingH3}
+      </h3>
+      <h4 className="typeSizerTarget" data-name="typeHeadingH4">
+        H4, {typeSizes.typeHeadingH4}
+      </h4>
 
       <h3>Body Copy</h3>
-      <Lead ref={lead}>P, {leadSize} Lead body copy.</Lead>
+      <Lead className="typeSizerTarget" data-name="typeBodyLead">
+        P, {typeSizes.typeBodyLead} Lead body copy.
+      </Lead>
 
-      <p ref={primary}>P, {primarySize} Primary body copy.</p>
+      <p className="typeSizerTarget" data-name="typeBodyPrimary">
+        P, {typeSizes.typeBodyPrimary} Primary body copy.
+      </p>
 
-      <Supplemental ref={supplemental}>P, {supplementalSize} Supplemental body copy.</Supplemental>
+      <Supplemental className="typeSizerTarget" data-name="typeBodySupplemental">
+        P, {typeSizes.typeBodySupplemental} Supplemental body copy.
+      </Supplemental>
 
       <h3>Other</h3>
-      <Stat ref={stat}>P, {statSize} Stat.</Stat>
+      <Stat className="typeSizerTarget" data-name="typeStat">
+        P, {typeSizes.typeStat} Stat.
+      </Stat>
 
-      <Label ref={label}>P, {labelSize} Label.</Label>
+      <Label className="typeSizerTarget" data-name="typeLabel">
+        P, {typeSizes.typeLabel} Label.
+      </Label>
 
       <hr />
     </StyledGuide>
   );
 }
 
-function useTypeResizer() {
-  const h1 = useRef(null);
-  const h2 = useRef(null);
-  const h3 = useRef(null);
-  const h4 = useRef(null);
-  const lead = useRef(null);
-  const primary = useRef(null);
-  const supplemental = useRef(null);
-  const stat = useRef(null);
-  const label = useRef(null);
-
-  const [h1Size, setH1Size] = useState(0);
-  const [h2Size, setH2Size] = useState(0);
-  const [h3Size, setH3Size] = useState(0);
-  const [h4Size, setH4Size] = useState(0);
-  const [leadSize, setLeadSize] = useState(0);
-  const [primarySize, setPrimarySize] = useState(0);
-  const [supplementalSize, setSupplementalSize] = useState(0);
-  const [statSize, setStatSize] = useState(0);
-  const [labelSize, setLabelSize] = useState(0);
+function useTypeSizing() {
+  const [typeSizes, setTypeSizes] = useState({});
 
   useEffect(() => {
     handleResize();
@@ -128,45 +194,21 @@ function useTypeResizer() {
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   function handleResize() {
-    setH1Size(getFontSize(h1.current));
-    setH2Size(getFontSize(h2.current));
-    setH3Size(getFontSize(h3.current));
-    setH4Size(getFontSize(h4.current));
-    setLeadSize(getFontSize(lead.current));
-    setPrimarySize(getFontSize(primary.current));
-    setSupplementalSize(getFontSize(supplemental.current));
-    setStatSize(getFontSize(stat.current));
-    setLabelSize(getFontSize(label.current));
+    const typeStyles = document.getElementsByClassName('typeSizerTarget');
+    for (const style of typeStyles) {
+      setTypeSizes((sizes) => {
+        const { name } = style.dataset;
+        sizes[name] = getFontSize(style);
+        return { ...sizes };
+      });
+    }
   }
 
   function getFontSize(element) {
     return parseInt(getComputedStyle(element)['font-size'], 10);
   }
 
-  return {
-    typeRefs: {
-      h1,
-      h2,
-      h3,
-      h4,
-      lead,
-      primary,
-      supplemental,
-      stat,
-      label,
-    },
-    typeSizes: {
-      h1Size,
-      h2Size,
-      h3Size,
-      h4Size,
-      leadSize,
-      primarySize,
-      supplementalSize,
-      statSize,
-      labelSize,
-    },
-  };
+  return typeSizes;
 }
 
 export default Styleguide;
