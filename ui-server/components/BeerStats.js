955ed5fe28017b97408c1ed7357efacf8877e34e
Bryan Downing <bryan.downing@idean.com>
2020-04-16 23:07:15 -0700

    Redo BeerStats layout

--- ui-server/components/BeerStats.js
+++ ui-server/components/BeerStats.js
@@ -1,8 +1,37 @@
 import styled from 'styled-components';
-import { typeBodyPrimary } from '../styles/typography';
 
-export default styled.dl`
-  ${typeBodyPrimary()}
-  grid-area: stats;
-  margin: 0;
+import BeerStat from './BeerStat';
+
+import { typeSizeBodyXS } from '../styles/typography';
+
+export default styled.div`
+  ${typeSizeBodyXS}
+  ${({ theme }) => theme.fontStackMono || null};
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: flex-end;
+  justify-content: flex-start;
+  width: calc(10ch + 3.8333333334em);
+  height: 5.333334em;
+
+  ${BeerStat} {
+    width: calc(10ch + 1.83333em);
+    margin-bottom: 0.5em;
+    margin-left: 2em;
+
+    &:last-child {
+      margin-top: 0.5em;
+      margin-bottom: auto;
+    }
+  }
+
+  @media (min-width: 600px) {
+    justify-content: space-between;
+    width: calc(20ch + 7.6666666667em);
+
+    ${BeerStat} {
+      margin-bottom: 0;
+    }
+  }
 `;
